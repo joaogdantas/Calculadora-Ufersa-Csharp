694e7d9d388880ef1d28644a6fe66960f2616d1e
João Gabriel Dantas <joaogdantass@gmail.com>
2022-03-16 18:29:36 -0300

    Versão 1.0

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace calculadoraufersa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float nota1 = 0;
+            float nota2 = 0;
+            float nota3 = 0;
+
+            Console.WriteLine("\nColoque sua primeira nota");
+            nota1 = float.Parse(Console.ReadLine() ?? "0");
+            do
+            {
+                if (nota1 < 0 || nota1 > 10)
+                {
+                    Console.WriteLine("Nota Inválida, insira sua primeira nota");
+                    nota1 = float.Parse(Console.ReadLine() ?? "0");
+                }
+                else {continue;}
+            } while (nota1 < 0 || nota1 > 10);
+
+            Console.WriteLine("\nColoque sua segunda nota");
+            nota2 = float.Parse(Console.ReadLine() ?? "0");
+            do
+            {
+                if (nota2 < 0 || nota2 > 10)
+                {
+                    Console.WriteLine("Nota Inválida, insira sua segunda nota");
+                    nota2 = float.Parse(Console.ReadLine() ?? "0");
+                }
+                else {continue;}
+            } while (nota2 < 0 || nota2 > 10);
+            
+            Console.WriteLine("\nColoque sua terceira nota");
+            nota3 = float.Parse(Console.ReadLine() ?? "0");
+            do
+            {
+                if (nota3 < 0 || nota3 > 10)
+                {
+                    Console.WriteLine("Nota Inválida, insira sua segunda nota");
+                    nota3 = float.Parse(Console.ReadLine() ?? "0");
+                }
+                else {continue;}
+            } while (nota3 < 0 || nota3 > 10);
+            
+            float mp = ((nota1 + nota2 + nota3)/3);
+            float af = ((50-6*mp)/4);
+
+            if (mp < 3.45)
+            {
+                Console.WriteLine("Você não atingiu a média necessária e foi reprovado. Sinto muito :(");
+            }
+            else if (mp >= 3.45 || mp < 6.95)
+            {
+                Console.WriteLine("Você não atingiu a média para passar por média e terá que fazer a quarta prova.\n");
+                Console.WriteLine("A nota que você precisa na quarta prova é: " + af);
+            }
+            else
+            {
+                Console.WriteLine("Você passou!");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Calculadora-Ufersa-Csharp
+#Calculadora desenvolvida para calcular a média do aluno nas matérias da UFERSA e a nota necessária em uma eventual quarta prova (Recuperação).#
+
+##Sistema de Cálculo de notas UFERSA##
+
+- MÉDIA PARCIAL (MP) será calculada pela MÉDIA ARITMÉTICA SIMPLES dos rendimentos acadêmicos obtidos em cada unidade: MP= (A1+A2+A3)/3. Sendo: A1= nota obtida na 1ª unidade, A2= nota obtida na 2ª unidade e A3= nota obtida na 3ª unidade;
+- Ao discente que apresentar MP maior ou igual que 3,5 (três e meio) e menor que 7,0 (sete) é assegurada a realização de uma AVALIAÇÃO FINAL, desde que tenha comparecido ao mínimo de 75% (setenta e cinco por cento) das atividades ministradas no componente curricular;
+- Ao discente que faltar a avaliação final será atribuída nota 0,0 (zero) e sua média será calculada utilizando a equação para MÉDIA FINAL;
+- A MÉDIA FINAL (MF) será calculada pela MÉDIA ARITMÉTICA PONDERADA da média parcial (MP) e da Avaliação Final (AF): MF=(MP X 6 + AF X4) /10. Será considerado APROVADO POR MÉDIA no componente curricular, o discente que obtiver MÉDIA PARCIAL igual ou superior a 7,0 (sete), desde que compareça a no mínimo 75% (setenta e cinco por cento) das atividades ministradas no componente curricular. Será considerado APROVADO POR AVALIAÇÃO FINAL no componente curricular, o discente que obtiver MÉDIA FINAL igual ou superior a 5,0 (cinco), desde que compareça a no mínimo 75% (setenta e cinco por cento) das atividades ministradas no componente curricular;
+- O discente estará REPROVADO no componente curricular quando obtiver uma MÉDIA PARCIAL MENOR QUE 3,5 (três e meio) ou uma MÉDIA FINAL MENOR QUE 5,0 (cinco) ou não comparecer no mínimo 75% (setenta e cinco por cento) das atividades ministradas no componente curricular;
